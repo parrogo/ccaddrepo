a7647ff85c52142dc36fcb7bb6c11c0daa4a6817
Andrea Parodi <andrea@parro.it>
2021-04-01 16:46:21 +0200

    add comments

--- ccaddrepo.go
+++ ccaddrepo.go
@@ -6,3 +6,19 @@ package ccaddrepo
 func Func() int {
 	return 42
 }
+
+// echo '{"data":{"type": "repos","attributes": {"url": "https://github.com/'${GITHUB_REPOSITORY}'"}}}' > body.json
+// curl -s \
+//   -H "Accept: application/vnd.api+json" \
+//   -H "Authorization: Token token=${CODE_CLIMATE_TOKEN}" \
+//   -H "Content-Type: application/vnd.api+json" \
+//   -d @body.json \
+//   https://api.codeclimate.com/v1/github/repos > .codeclimate.json
+
+//       - name: Test & publish code coverage
+//         uses: paambaati/codeclimate-action@v2.7.5
+//         env:
+//           CC_TEST_REPORTER_ID: ${---{ secrets.CC_TEST_REPORTER_ID }}
+//         with:
+//           coverageCommand: go test -coverprofile=c.out ./...
+//           prefix: github.com/{{.Author}}/{{.RepoName}}
